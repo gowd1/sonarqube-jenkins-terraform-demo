01f3fae10ab2e07303b6c16ab71eb2ae7a092ad0
arun <arun@gmail.com>
2023-03-17 18:42:48 +0530

    new

--- instance.tf
+++ instance.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "web" {
   count                       = var.inst_count
   availability_zone           = "us-east-1c"
   key_name                    = "arun"
-  vpc_security_group_ids      = [aws_security_group.allow_http_ssh.id]
+  vpc_security_group_ids      = [aws_security_group.allow_http_ssh]
 
   tags = var.ec2_instance_tags
 }
